67c6cb731e42ac784c104fa969714c7781f9eb87
CAROL SANCHES <135077050+CarolinaSanches24@users.noreply.github.com>
2024-04-10 18:13:44 -0300

    feat: add ResolverCustom

--- src/resolvers/appoiments-resolver.ts
+++ src/resolvers/appoiments-resolver.ts
@@ -1,12 +1,25 @@
-import { Query, Resolver, Mutation, Arg } from "type-graphql";
+import {
+  Query,
+  Resolver,
+  Mutation,
+  Arg,
+  FieldResolver,
+  Root,
+} from "type-graphql";
 import { CreateAppoimentInput } from "../dtos/inputs/create-appoiments-input";
 import { Appoiment } from "../dtos/models/appoiments-model";
+import { Customer } from "../dtos/models/customer.model";
 
-@Resolver()
+@Resolver(() => Appoiment)
 export class AppoimentsResolver {
-  @Query(() => String)
-  async helloWorld() {
-    return "hello World";
+  @Query(() => [Appoiment])
+  async appoiments() {
+    return [
+      {
+        startsAt: new Date(),
+        endsAt: new Date(),
+      },
+    ];
   }
 
   @Mutation(() => Appoiment)
@@ -16,5 +29,14 @@ export class AppoimentsResolver {
       endsAt: data.endsAt,
     };
     return appoiment;
+  }
+
+  @FieldResolver(() => Customer)
+  async customer(@Root() appoiment: Appoiment) {
+    console.log(appoiment);
+
+    return {
+      name: "Jonh Doe",
+    };
   }
 }
